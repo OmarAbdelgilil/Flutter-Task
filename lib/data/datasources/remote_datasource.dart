05ec60e85e3e00f7020d1b6000c12ad842a0bbd1
Omar <Omar Abdelgilil007>
2024-09-15 15:41:43 +0300

    remote datasource created

---
+++ lib/data/datasources/remote_datasource.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:fake_products/data/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class RemoteDataSource {
+  final String apiUrl = "https://fakestoreapi.com/products";
+
+  Future<List<Product>> fetchProducts() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => Product.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+}
